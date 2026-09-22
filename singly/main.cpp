540234c3aa80b4faa7d1af2098dad514d86dfd6a
Ngoc <ngoc.nguyen1511@hcmut.edu.vn>
2026-09-22 10:25:37 +0700

    Insert new node at start

--- singly/main.cpp
+++ singly/main.cpp
@@ -8,6 +8,13 @@ class Node{
     Node(int v){
         value = v;
         next = NULL;
+    /*Cách viết 2: 
+    Node(int value){
+        this->value = value;
+        this->next = NULL;
+    }*/
+    /*Cách viết 3:
+    Node (int value) : value(value), next(NULL) {}*/
     }
 };
 
@@ -19,10 +26,18 @@ void traverse(Node* head){
     } cout <<"NULL"<<endl;
 }
 
+void insertAtHead(Node* &head, int val){
+    Node* newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+
 int main(){
     Node* node1 = new Node(1);
     Node* node2 = new Node(2);
     node1->next = node2;
     Node* head = node1;
+    traverse(head);
+    insertAtHead(head, 3);
     traverse(head);
 }
